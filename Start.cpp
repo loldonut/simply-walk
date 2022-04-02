dde6d8d1be3f0fb2eedae2a946250a81641a390c
loldonut <loldonutph@gmail.com>
2022-04-02 09:06:19 +0800

    minimize str to int process in `Start`

--- Start.cpp
+++ Start.cpp
@@ -9,10 +9,9 @@
 void Start(int argc, char** argv)
 {
     std::string WalkLimitString = argc > 1 ? argv[1] : "400";
-    int WalkLimit = 400;
-
-    if (argc >= 1)
-        WalkLimit = stoi(WalkLimitString);
+    int WalkLimit = WalkLimitString != "400" 
+        ? stoi(WalkLimitString)
+        : 400;
 
     Player player;
     if (WalkLimit != 400)
